587a19e8c8eacdbd17398f46618a1098a6850170
kkllkk5 <krb1008@icloud.com>
2025-07-12 21:34:53 +0900

    sleep対策のため，裏でサーバを常に起動するよう修正

---
+++ run_all.sh
@@ -0,0 +1,6 @@
+# 両方をバックグラウンドで起動しつつ監視
+
+python server.py &
+python bot.py
+
+# discord_bot.py を終了させたらコンテナも終了
--- server.py
+++ server.py
@@ -3,6 +3,10 @@ from threading import Thread
 from fastapi import FastAPI
 import uvicorn
 import os
+import dotenv
+import asyncio
+
+dotenv.load_dotenv()
 
 app = FastAPI()
 
@@ -13,6 +17,7 @@ async def root():
 
 
 def start():
+    asyncio.set_event_loop(asyncio.new_event_loop())
     port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
 
@@ -20,3 +25,6 @@ def start():
 def server_thread():
     t = Thread(target=start)
     t.start()
+
+if __name__ == '__main__':
+    server_thread()
