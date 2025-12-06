95f9793c79bd002afee316152025e6004c5155c9
kkllkk5 <krb1008@icloud.com>
2025-12-06 21:13:13 +0900

    曲名検索まで作成

--- bot.py
+++ bot.py
@@ -33,10 +33,20 @@ async def on_message(message):
                 int(level), int(song_num))
             response = "以下が課題曲です!\n"
             for music in practice_music_list:
-                response += (music)
+                response += (music[0])
         except Exception as e:
             response = "レベルには11,12のみを現在サポートしています"
         await message.channel.send(response)
+
+    # 「/dp_level {曲名の一部}」と送ると，指定した曲のDP非公式難易度を答える
+    # 曲名の一部から候補を複数提示し，その中から番号を指定して指定楽曲を特定する
+    if re.match('/dp_level .*',message.content):
+        _, song_name = message.content.split()
+        candidate_list = iidx.search_songname_for_dp(song_name)
+        response = candidate_list
+        await message.channel.send(response)
+
+
 
 # Botの起動とDiscordサーバーへの接続
 client.run(token)
--- feature/iidx.py
+++ feature/iidx.py
@@ -1,21 +1,23 @@
 import json
 import csv
 import random
+import re
 
 # CSVファイルを各行ごとのリストへと変換
-
 
 def csvToList(csvName):
     list = []
     with open(csvName, mode='r', encoding='utf-8') as csv_file:
         for row in csv_file:
-            list.append(row)
+            list_in_row = []
+            for item in row.split(','):
+                list_in_row.append(item)
+            list.append(list_in_row)
     return list
 
 # 課題曲をランダムに選択
 # level:対象レベル（現状11,12のみサポート）
 # song_num:課題曲の曲数
-
 
 def make_practice_music(level, song_num):
     if level == 11:
@@ -25,3 +27,16 @@ def make_practice_music(level, song_num):
     else:
         raise Exception("課題曲生成は現在レベル11,12のみ対応しています")
     return random.choices(music_list, k=song_num)
+
+# DPの楽曲リストから，楽曲を検索する
+# name_parts:曲名の一部
+def search_songname_for_dp(name_parts):
+    candidate_list = []
+    # DP楽曲リストから，曲名を取得
+    # (レベル情報も同時に取得されるが，今回は利用しない)
+    music_list = csvToList('dp_level.csv')
+    for music_info in music_list:
+        music_name = music_info[0]
+        if re.match('.*' + name_parts + '.*',music_name):
+            candidate_list.append(music_name)
+    return candidate_list
