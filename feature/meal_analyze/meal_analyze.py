e879f186e59335551cf3c3632933f5c0c5dd2916
kkllkk5 <krb1008@icloud.com>
2026-09-25 12:16:47 +0900

    入力トークン制限を取り消し

--- feature/meal_analyze/meal_analyze.py
+++ feature/meal_analyze/meal_analyze.py
@@ -111,9 +111,6 @@ def analyze_meal_images(images: list[tuple[bytes, str]], text:str, user_name:str
             gemini.types.Part.from_bytes(
                 data=image_bytes, 
                 mime_type=mime_type,
-                media_resolution={
-                    "level": "media_resolution_medium",  # 低解像度で送信することで、geminiの処理速度を向上させる
-                },
                 )
             )
 
